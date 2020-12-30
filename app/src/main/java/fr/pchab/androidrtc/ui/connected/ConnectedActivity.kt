d9d6c3ea89adcf9e6f27e23a296e2db722256258
Sam Nguyen <sam@Sams-MacBook-Pro.local>
2020-12-30 11:16:50 +0700

    added

---
+++ app/src/main/java/fr/pchab/androidrtc/ui/connected/ConnectedActivity.kt
@@ -0,0 +1,28 @@
+package fr.pchab.androidrtc.ui.connected
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import fr.pchab.androidrtc.R
+import fr.pchab.androidrtc.base.BaseActivity
+import kotlinx.android.synthetic.main.common_top_bar.*
+
+class ConnectedActivity : BaseActivity() {
+	companion object {
+		fun start(
+			context: Context?
+		) {
+			val intent = Intent(context, ConnectedActivity::class.java)
+			intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+			context?.startActivity(intent)
+		}
+	}
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_connected)
+
+		initToolBar(toolbar)
+		tvTitle?.setText(R.string.connected)
+	}
+}
